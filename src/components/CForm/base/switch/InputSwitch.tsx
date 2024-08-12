27da65585cdc5baa7b55c441bd06c847af948c84
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-12 14:22:11 +0330

    feat:label for switch

--- src/components/CForm/base/switch/InputSwitch.tsx
+++ src/components/CForm/base/switch/InputSwitch.tsx
@@ -1,27 +1,37 @@
 // import React from "react";
 import { ChangeEvent, FC } from "react";
 import switchClass from "./switch.module.css";
-import { ConfigFormIF, FormCForm, StringObject } from "../interFaceCForm";
+import { FormCForm } from "../interFaceCForm";
 interface InputSwitchIF {
   ele: FormCForm;
-  formError: StringObject;
-  config: ConfigFormIF;
-  setFormValues: any;
-  change: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  change: (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    type: "checked"
+  ) => void;
 }
-export const InputSwitch: FC<InputSwitchIF> = ({
-  ele,
-  formError,
-  config,
-  setFormValues,
-  change,
-}) => {
+export const InputSwitch: FC<InputSwitchIF> = ({ ele, change }) => {
   return (
-    <div>
-      <label className={switchClass.switch}>
-        <input type="checkbox"  id={ele.name} onChange={(e) =>{ change(e);console.log('{e}',e.target.checked)} } />
-        <span className={switchClass.slider}></span>
-      </label>
+    <div style={{ display: "flex", alignItems: "center",gap:'1rem' }}>
+      {ele?.label && (
+        <label
+          // style={{ ...config?.labelStyle, ...ele?.labelStyle }}
+          htmlFor={ele.name}
+        >
+          {ele?.label}
+        </label>
+      )}
+      <div>
+        <label className={switchClass.switch}>
+          <input
+            type="checkbox"
+            id={ele.name}
+            onChange={(e) => {
+              change(e, "checked");
+            }}
+          />
+          <span className={switchClass.slider}></span>
+        </label>
+      </div>
     </div>
   );
 };
