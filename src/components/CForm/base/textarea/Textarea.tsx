263f60375b7d5aee0d0db7b04164e1f9a5630dff
Ch-alireza021 <ch.alireza021@gmail.com>
2024-08-13 15:38:18 +0330

    fixbug textarea

--- src/components/CForm/base/textarea/Textarea.tsx
+++ src/components/CForm/base/textarea/Textarea.tsx
@@ -1,14 +1,27 @@
 // import React from "react";
-import { ChangeEvent, FC } from "react";
+import { ChangeEvent, FC, useContext } from "react";
 import { FormCForm } from "../interFaceCForm";
+import { MyContext } from "../../helpers";
 interface TextareaIF {
   ele: FormCForm;
   change: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 export const Textarea: FC<TextareaIF> = ({ ele, change }) => {
+  const { state } = useContext(MyContext);
   return (
-    <textarea id={ele.name} name={ele.name} value={''} onChange={(e) => change(e)} rows={5}  style={{width:'100%'}}>
-   {ele?.label}
+    <textarea
+      id={ele.name}
+      name={ele.name}
+      value={
+        state.formValues?.[ele.name] !== undefined
+          ? String(state.formValues[ele.name])
+          : ""
+      }
+      onChange={(e) => change(e)}
+      rows={5}
+      style={{ width: "100%" }}
+    >
+      {ele?.label}
     </textarea>
   );
 };
